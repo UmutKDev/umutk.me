1a6b91a3a67a6f00a85ed405bd5538a3839797bb
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-04 20:17:54 +0000

    blur added

--- src/components/layouts/sidebar/avatar/Image.tsx
+++ src/components/layouts/sidebar/avatar/Image.tsx
@@ -2,6 +2,11 @@ const Image = (): JSX.Element => {
   return (
     <figure className="relative">
       <img src="img/umutk.jpg" className="w-24 rounded" draggable={false} />
+      <img
+        src="img/umutk.jpg"
+        className="lef-0 absolute top-0 bottom-0 right-0 -z-10  w-24 rounded blur-xl"
+        draggable={false}
+      />
       <div className="absolute -bottom-1 -right-1 h-4 w-4 rounded-md bg-green-500"></div>
     </figure>
   );

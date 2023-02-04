ffb800977d77dd3f2182b56b9cc1f2088f06c441
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-04 14:40:04 +0000

    links to navigation

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,12 +1,12 @@
 import Avatar from "src/components/layouts/sidebar/avatar";
-import Links from "./sidebar/Navigation";
+import Navigation from "./sidebar/Navigation";
 import Presence from "./sidebar/presence";
 
 const Sidebar = (): JSX.Element => {
   return (
     <aside className="hidden h-screen w-60 border-r border-[#1C1C1C] lg:block">
       <Avatar />
-      <Links />
+      <Navigation />
       <Presence />
     </aside>
   );

40681751363686c7cd9af1b2425f51209722d53e
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-04 15:35:51 +0000

    status dummy icon

--- src/components/layouts/sidebar/avatar/Image.tsx
+++ src/components/layouts/sidebar/avatar/Image.tsx
@@ -1,7 +1,8 @@
 const Image = (): JSX.Element => {
   return (
-    <figure>
-      <img src="img/umutk.jpg" className="w-24 rounded" />
+    <figure className="relative">
+      <img src="img/umutk.jpg" className="w-24 rounded" draggable={false} />
+      <div className="absolute -bottom-1 -right-1 h-4 w-4 rounded-md bg-green-500"></div>
     </figure>
   );
 };

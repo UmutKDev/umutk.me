306fec11ea4833c35504f0a21e4a67be5101304d
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-04 15:27:57 +0000

    connections added

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,12 +1,14 @@
 import Avatar from "src/components/layouts/sidebar/avatar";
+import Connections from "./sidebar/connections";
 import Navigation from "./sidebar/Navigation";
 import Presence from "./sidebar/presence";
 
 const Sidebar = (): JSX.Element => {
   return (
-    <aside className="hidden h-screen w-60 border-r border-[#1C1C1C] lg:block">
+    <aside className="hidden h-full w-60 border-r border-[#1C1C1C] lg:flex lg:flex-col">
       <Avatar />
       <Navigation />
+      <Connections />
       <Presence />
     </aside>
   );

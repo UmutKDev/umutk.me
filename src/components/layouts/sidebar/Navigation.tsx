c9db64b17bcdb272a91919b4d2bcb6f1492a7cfb
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-05 23:25:12 +0000

    frame motion added for route change

--- src/components/layouts/sidebar/Navigation.tsx
+++ src/components/layouts/sidebar/Navigation.tsx
@@ -1,24 +1,76 @@
-"use client";
-
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Navigation as NavigationItems } from "@/libs/config";
 import useSound from "use-sound";
+import { motion, PanInfo } from "framer-motion";
 
 const Navigation = (): JSX.Element => {
   const pathname = usePathname();
+  const history = useRouter();
+
+  const pathnameOffsets: { [key: string]: number } = useMemo(
+    () =>
+      NavigationItems.reduce(
+        (acc, cur, index) => ({ ...acc, [cur.path]: index * 56 }),
+        {}
+      ),
+    []
+  );
+
+  const dragConstraintsRef = useRef(null);
+
+  const [dragYOffset, setDragYOffset] = useState(0);
 
   const [playSwitchPageSound] = useSound("/sounds/switch-page.mp3");
   useEffect(() => {
     playSwitchPageSound();
-  }, [pathname]);
+  }, [pathname, playSwitchPageSound]);
+
+  const pageIndicatorOffset = useMemo(
+    () => (pathname ? pathnameOffsets[pathname] ?? -180 : 0),
+    [pathname, pathnameOffsets]
+  );
+
+  const pageIndicatorOffsetWithDecoration = useMemo(
+    () => pageIndicatorOffset - dragYOffset,
+    [pageIndicatorOffset, dragYOffset]
+  );
+
+  const onPageIndicatorDragEnd = useCallback(
+    (_event: PointerEvent, info: PanInfo) => {
+      const goal = pageIndicatorOffset + info.offset.y;
+
+      const closest = Object.entries(pathnameOffsets).reduce(
+        ([prevPath, prevOffset]: any, [curPath, curOffset]: any) => {
+          return Math.abs(curOffset - goal) < Math.abs(prevOffset - goal)
+            ? [curPath, curOffset]
+            : [prevPath, prevOffset];
+        }
+      );
+
+      if (closest[0] === pathname) return;
+
+      setDragYOffset(dragYOffset + info.offset.y + info.velocity.y);
+      history.push(closest[0]);
+    },
+    [pageIndicatorOffset, pathnameOffsets, pathname, dragYOffset, history]
+  );
 
   return (
     <div className="relative flex h-[calc(100vh-26.5rem)] w-full">
+      <motion.div
+        whileHover={{ width: 3 }}
+        drag="y"
+        onDragEnd={onPageIndicatorDragEnd}
+        dragConstraints={dragConstraintsRef}
+        animate={{ top: pageIndicatorOffsetWithDecoration }}
+        className="absolute top-0 -right-[1px] h-[58px] w-[1px] bg-white/80"
+      />
       <ul className="flex w-full flex-col items-center divide-y divide-dashed divide-[#1c1c1c]">
         {NavigationItems.map((link) => (
           <li
+            ref={dragConstraintsRef}
             key={link.name}
             className={`links border-green-400 last:!border-b`}
           >

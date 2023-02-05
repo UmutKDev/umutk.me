44ff51c507c91a57192650d98fa5e9399605e445
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-05 23:25:01 +0000

    frame motion added for route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
 import Sidebar from "@/components/layouts/Sidebar";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -10,14 +12,25 @@ const poppins = Poppins({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
       <Sidebar className={poppins.className} />
-      <main
-        className={`${poppins.className} w-[calc(100vw-15rem)] py-12 px-8 `}
-      >
-        <Component {...pageProps} />
-      </main>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 2 }}
+          transition={{ duration: 0.85 }}
+        >
+          <main
+            className={`${poppins.className} w-[calc(100vw-15rem)] py-12 px-8 `}
+          >
+            <Component {...pageProps} />
+          </main>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }

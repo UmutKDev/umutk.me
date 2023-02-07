eaaaca0659f70fa6d66e36c50a79272c5d28d9bc
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-07 23:07:56 +0000

    mb added

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -7,7 +7,7 @@ const Main = ({
 }) => {
   return (
     <section>
-      <h1 className="text-3xl font-semibold underline decoration-sky-500/80 decoration-2 underline-offset-4">
+      <h1 className="mb-4 text-3xl font-semibold underline decoration-sky-500/80 decoration-2 underline-offset-4">
         {title}
       </h1>
       {children}

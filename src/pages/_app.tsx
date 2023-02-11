50ae28e5d08e4d90ffe1217b4fe254319dee13db
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-11 02:23:50 +0000

    snowfall and intro added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import "@/styles/globals.css";
+import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
-import { Poppins } from "@next/font/google";
-import Sidebar from "@/components/layouts/Sidebar";
-import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import "@/styles/globals.css";
+import { Poppins } from "@next/font/google";
+import { motion, AnimatePresence } from "framer-motion";
+import Sidebar from "@/components/layouts/Sidebar";
+import Intro from "@/components/layouts/Intro";
+import Snowfall from "react-snowfall";
+import { NEXT_SEO_DEFAULT } from "@/libs/config";
+import { NextSeo } from "next-seo";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -11,11 +16,33 @@ const poppins = Poppins({
   display: "swap",
 });
 
+const isWinter = new Date().getMonth() >= 10 || new Date().getMonth() <= 2;
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const [isIntro, setIsIntro] = useState(true);
 
-  return (
+  useEffect(() => {
+    setTimeout(() => setIsIntro(false), 1500);
+  }, []);
+
+  return isIntro ? (
     <>
+      <NextSeo {...NEXT_SEO_DEFAULT} defaultTitle={"Umut Kızıloğlu"} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 2 }}
+          transition={{ duration: 0.85 }}
+        >
+          <Intro setIsIntro={setIsIntro} />
+        </motion.div>
+      </AnimatePresence>
+    </>
+  ) : (
+    <>
+      {isWinter && <Snowfall snowflakeCount={20} speed={[0.5, 1]} />}
       <Sidebar className={poppins.className} />
       <AnimatePresence mode="wait">
         <motion.div

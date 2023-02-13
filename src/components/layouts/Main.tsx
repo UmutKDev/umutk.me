265ff998031d80f6decc4071f184735094fd6e78
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-13 22:42:09 +0000

    id added to main component

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -6,7 +6,7 @@ const Main = ({
   title: string;
 }) => {
   return (
-    <section>
+    <section id={title.toLocaleLowerCase()}>
       <h1 className="mb-4 text-3xl font-semibold underline decoration-sky-500/80 decoration-2 underline-offset-4">
         {title}
       </h1>

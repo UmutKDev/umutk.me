8a45651583c0beb4804d23e5c9bc3d6efab33fca
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-13 22:42:28 +0000

    projects changed stuff

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,14 @@
 import Main from "@/components/layouts/Main";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
+import { Poppins, Courier_Prime } from "@next/font/google";
 import { Projects as ProjectsItems, NEXT_SEO_DEFAULT } from "@/libs/config";
+
+const curriorPrime = Courier_Prime({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Projects(): JSX.Element {
   return (
@@ -9,21 +16,25 @@ export default function Projects(): JSX.Element {
       <NextSeo {...NEXT_SEO_DEFAULT} title="Projects" />
       <Main title="Projects">
         <div className="w-full">
-          <ul>
-            <Link href="#">
-              <li className="grid h-24 w-full grid-cols-12 rounded-md border border-[#1C1C1C] bg-transparent transition-all duration-200 ease-in-out hover:bg-white/10">
-                <div className="col-span-2 flex flex-col items-start justify-center border-r border-[#1C1C1C] pl-5">
-                  <span style={{ fontFamily: "" }}>name</span>
-                  <span>umutk.me</span>
-                </div>
-                <div className="col-span-2 flex items-center justify-start border-r border-[#1C1C1C] pl-5">
-                  Typescript
-                </div>
-                <div className="col-span-8 flex items-center justify-start pl-5">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </div>
-              </li>
-            </Link>
+          <ul className="flex flex-col gap-y-4">
+            {ProjectsItems.map((project, i) => (
+              <Link href="#" key={i}>
+                <li className="grid h-24 w-full grid-cols-12 rounded-md border border-[#1C1C1C] bg-transparent transition-all duration-200 ease-in-out hover:bg-white/10">
+                  <div className="col-span-2 flex flex-col items-start justify-center border-r border-[#1C1C1C] pl-5">
+                    <p className={curriorPrime.className}>name</p>
+                    <p className="font-light text-white/80">umutk.me</p>
+                  </div>
+                  <div className="col-span-2 flex flex-col items-start justify-center border-r border-[#1C1C1C] pl-5">
+                    <p className={curriorPrime.className}>language</p>
+                    <p className="font-light text-white/80 flex items-center">typescript <div className="ml-3 w-3 h-3 rounded-full bg-blue-800"></div> </p>
+                  </div>
+                  <div className="col-span-8 flex flex-col items-start justify-center pl-5">
+                    <p className={curriorPrime.className}>description</p>
+                    <p className="font-light text-white/80">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  </div>
+                </li>
+              </Link>
+            ))}
           </ul>
         </div>
 

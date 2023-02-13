cf99f8f7b8be556d45597b84aac759829afff41d
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-13 22:42:18 +0000

    return to old intro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,12 +26,25 @@ export default function App({ Component, pageProps }: AppProps) {
     setTimeout(() => setIsIntro(false), 1500);
   }, []);
 
-  return (
+  return isIntro ? (
+    <>
+      <NextSeo {...NEXT_SEO_DEFAULT} defaultTitle={"Umut Kızıloğlu"} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 2 }}
+          transition={{ duration: 0.85 }}
+        >
+          <Intro />
+        </motion.div>
+      </AnimatePresence>
+    </>
+  ) : (
     <>
       {isWinter && <Snowfall snowflakeCount={20} speed={[0.5, 1]} />}
       <Sidebar className={poppins.className} />
       <AnimatePresence mode="wait">
-      {isIntro && <Intro setIsIntro={setIsIntro} />}
         <motion.div
           key={router.route}
           initial={{ opacity: 0 }}

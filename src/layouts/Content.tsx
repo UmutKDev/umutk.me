6a267cdf0f04080d6df62975a7ca23be1c79c2f6
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-03-10 21:28:20 +0000

    responsive fixes

--- src/layouts/Content.tsx
+++ src/layouts/Content.tsx
@@ -16,7 +16,12 @@ const Content = ({
   className?: string;
 }) => {
   return (
-    <main className={[poppins.className, "py-12 px-8 lg:w-[calc(100vw-15rem)]"].join(" ")}>
+    <main
+      className={[
+        poppins.className,
+        "w-screen py-12 px-8 lg:w-[calc(100vw-15rem)]",
+      ].join(" ")}
+    >
       <section id={title.toLocaleLowerCase()} className={className}>
         <h1 className="mb-4 text-3xl font-semibold underline decoration-sky-500/80 decoration-2 underline-offset-4">
           {title}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
-import type { AppProps } from "next/app";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
 import { motion, AnimatePresence } from "framer-motion";
 import Sidebar from "@/layouts/Sidebar";
-import styled from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -14,32 +13,47 @@ export default function App({ Component, pageProps }: AppProps) {
     [openOnMobile]
   );
 
+  useEffect(() => {
+    if (document.body.clientWidth < 850) {
+      toggleMobileMenu();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.route]);
+
+  useEffect(() => {
+    document.body.style.overflow = openOnMobile ? "unset" : "hidden";
+  }, [openOnMobile]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (document.body.clientWidth > 850) {
+        setOpenOnMobile(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [router.route]);
+
   return (
     <>
       <Sidebar
         openOnMobile={openOnMobile}
         toggleMobileMenu={toggleMobileMenu}
       />
-      <Container openOnMobile={openOnMobile}>
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={router.route}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 2 }}
-            transition={{ duration: 0.85 }}
-          >
-            <Component {...pageProps} />
-          </motion.div>
-        </AnimatePresence>
-      </Container>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 2 }}
+          transition={{ duration: 0.85 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
-
-const Container = styled.div<{ openOnMobile: boolean }>`
-  @media (max-width: 850px) {
-    width: 100%;
-    margin-top: ${({ openOnMobile }) => (openOnMobile ? "2rem" : "0")};
-    display: ${({ openOnMobile }) => (openOnMobile ? "block" : "none")};
-  }
-`;

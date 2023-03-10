7fb5967a97185f41db03150c16433bb1318b826c
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-03-10 04:53:37 +0000

    Avatar Name is dynamic

--- src/components/AvatarName.tsx
+++ src/components/AvatarName.tsx
@@ -1,5 +1,12 @@
+import { About } from "@/libs/config";
+
 const Name = (): JSX.Element => {
-  return <h1 className="text-xl font-bold">Umut Kızıloğlu</h1>;
+
+  const {
+    name: { firstname, lastname }
+  } = About;
+
+  return <h1 className="text-xl font-bold">{firstname} {lastname}</h1>;
 };
 
 export default Name;

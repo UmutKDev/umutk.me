b2e73739fd62ff6745749cd4959f23da3b462504
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-03-10 16:27:29 +0000

    Navigation Improved

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,22 +3,20 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { Navigation as NavigationItems } from "@/libs/config";
 import useSound from "use-sound";
-import { motion, PanInfo } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Navigation = ({ isActivityTrue }: React.PropsWithChildren<any>) => {
+  const { push } = useRouter();
   const pathname = usePathname();
-  const history = useRouter();
 
-  const pathnameOffsets: { [key: string]: number } = useMemo(
-    () =>
-      NavigationItems.reduce(
-        (acc, cur, index) => ({ ...acc, [cur.path]: index * 56 }),
-        {}
-      ),
-    []
-  );
+  const pathnameOffsets: { [key: string]: number } = useMemo(() => {
+    return NavigationItems.reduce(
+      (acc, cur, index) => ({ ...acc, [cur.path]: index * 56 }),
+      {}
+    );
+  }, []);
 
-  const dragConstraintsRef = useRef(null);
+  const dragConstraintsRef = useRef<HTMLLIElement>(null);
 
   const [dragYOffset, setDragYOffset] = useState(0);
 
@@ -27,46 +25,39 @@ const Navigation = ({ isActivityTrue }: React.PropsWithChildren<any>) => {
     playSwitchPageSound();
   }, [pathname, playSwitchPageSound]);
 
-  const pageIndicatorOffset = useMemo(
-    () => (pathname ? pathnameOffsets[pathname] ?? -180 : 0),
-    [pathname, pathnameOffsets]
-  );
+  const pageIndicatorOffset = useMemo(() => {
+    return pathname ? pathnameOffsets[pathname] ?? -180 : 0;
+  }, [pathname, pathnameOffsets]);
 
-  const pageIndicatorOffsetWithDecoration = useMemo(
-    () => pageIndicatorOffset - dragYOffset,
-    [pageIndicatorOffset, dragYOffset]
-  );
+  const pageIndicatorOffsetWithDecoration = useMemo(() => {
+    return pageIndicatorOffset - dragYOffset;
+  }, [pageIndicatorOffset, dragYOffset]);
 
   const onPageIndicatorDragEnd = useCallback(
-    (_event: PointerEvent, info: PanInfo) => {
+    (_event: any, info: { offset: { y: number }; velocity: { y: any } }) => {
       const goal = pageIndicatorOffset + info.offset.y;
-
       const closest = Object.entries(pathnameOffsets).reduce(
-        ([prevPath, prevOffset]: any, [curPath, curOffset]: any) => {
-          return Math.abs(curOffset - goal) < Math.abs(prevOffset - goal)
-            ? [curPath, curOffset]
-            : [prevPath, prevOffset];
-        }
+        (prev, cur) =>
+          Math.abs(cur[1] - goal) < Math.abs(prev[1] - goal) ? cur : prev,
+        [pathname, pageIndicatorOffset] as [string, number]
       );
 
       if (closest[0] === pathname) return;
 
-      setDragYOffset(dragYOffset + info.offset.y + info.velocity.y);
-      history.push(closest[0]);
+      setDragYOffset((prev) => prev + info.offset.y + info.velocity.y);
+      push(closest[0]);
     },
-    [pageIndicatorOffset, pathnameOffsets, pathname, dragYOffset, history]
+    [pathname, push, pageIndicatorOffset, pathnameOffsets]
   );
 
-  const checkIfActivityTrue = useCallback(() => {
-    if (isActivityTrue()) {
-      return "h-[calc(100vh-31em)] lg:h-[calc(100vh-26.5em)]";
-    } else {
-      return "h-[calc(100vh-26rem)] lg:h-[calc(100vh-20.5rem)]";
-    }
+  const containerHeight = useMemo(() => {
+    return isActivityTrue()
+      ? "h-[calc(100vh-31em)] lg:h-[calc(100vh-26.5em)]"
+      : "h-[calc(100vh-26rem)] lg:h-[calc(100vh-20.5rem)]";
   }, [isActivityTrue]);
 
   return (
-    <div className={`relative flex w-full ${checkIfActivityTrue()}`}>
+    <div className={`relative flex w-full ${containerHeight}`}>
       <motion.div
         whileHover={{ width: 3 }}
         drag="y"
@@ -76,13 +67,13 @@ const Navigation = ({ isActivityTrue }: React.PropsWithChildren<any>) => {
         className="absolute top-0 -right-[1px] h-[58px] w-[1px] bg-white/80"
       />
       <ul className="flex w-full flex-col items-center divide-y divide-dashed divide-[#1c1c1c]">
-        {NavigationItems.map((link) => (
+        {NavigationItems.map(({ name, path }) => (
           <li
             ref={dragConstraintsRef}
-            key={link.name}
+            key={name}
             className={`links last:!border-b`}
           >
-            <Link href={link.path}>{link.name}</Link>
+            <Link href={path}>{name}</Link>
           </li>
         ))}
       </ul>
